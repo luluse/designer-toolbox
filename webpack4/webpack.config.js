ad13689e382536aa9eddf29f6dfc39654e28bc15
LULU SEVIGNON <LUDIVINE.SEVIGNON@GMAIL.COM>
2021-06-23 12:19:34 -0700

    added multiple entry points

--- webpack4/webpack.config.js
+++ webpack4/webpack.config.js
@@ -1,10 +1,18 @@
 const path = require("path");
 
 module.exports = {
-    entry: "./src/index.js",
+    entry: {
+        about: "./src/about.js",
+        contact: "./src/contact.js"
+    },
     output: {
-        filename: "main.js",
+        filename: "[name].bundle.js",
         path: path.resolve(__dirname, "dist")
+    },
+    optimization: {
+      splitChunks: {
+          chunks: 'all'
+      }
     },
     devServer: {
         contentBase: path.join(__dirname, "dist"),
